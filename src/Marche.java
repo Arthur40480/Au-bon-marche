7cd87f069d530f0ec6505726690ea9dbd191dddb
Arthur Gibert <arthurgibert@free.fr>
2024-01-04 14:52:41 +0100

    Ajout d'un id pour chaques fruits et légumes

--- src/Marche.java
+++ src/Marche.java
@@ -15,6 +15,7 @@ public class Marche {
 		Object[][] informationBoard = {
 				{"Clémentine", "Datte", "Grenade", "Kaki", "Kiwi", "Mandarine", "Orange", "Pamplemousse", "Poire","Pomme", 		//  <- Noms
 				"Carrote", "Choux de Bruxelles", "Chou vert", "Courge butternut", "Endive", "Epinard", "Poireau", "Potiron", "Radis noir", "Salsifis"},	
+				{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},	// <- Id
 				{6, 4, 3, 3, 5, 6, 8, 8, 5, 8, 7, 4, 12, 6, 5, 4, 5, 6, 10, 3},		//  <- Stock
 				{2.90, 7, 3.50, 4.50, 3.50, 2.80, 1.50, 2, 2.50, 1.50, 1.30, 4, 2.50, 2.50, 2.50, 2.60, 1.20, 2.50, 5, 2.50},	//  <- Prix
 				{true, true, true, true, true, true, true, false, true, true, true, true, false, false, true, true, true, false, false, true},	//  <- Prix au kg ?
@@ -24,9 +25,10 @@ public class Marche {
 		for(int i = 0; i < 20; i++) {
 			HashMap<String, Object> merchandise = new HashMap<>();
 			merchandise.put("Nom", informationBoard[0][i]);
-			merchandise.put("Stock", informationBoard[1][i]);
-			merchandise.put("Prix", informationBoard[2][i]);
-			merchandise.put("PrixAuKg", informationBoard[3][i]);
+			merchandise.put("Id", informationBoard[1][i]);
+			merchandise.put("Stock", informationBoard[2][i]);
+			merchandise.put("Prix", informationBoard[3][i]);
+			merchandise.put("PrixAuKg", informationBoard[4][i]);
 			
 			// On ajoute la HashMap à la ArrayList :
 			merchandiseList.add(merchandise);
