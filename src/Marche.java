ebedbc944c54343ab02339400b577e959ca9612f
Arthur Gibert <arthurgibert@free.fr>
2024-01-05 13:45:22 +0100

    Ajout de la méthode updateCart qui permet de mettre à jour le panier du client après chaque ajout dans le panier.

--- src/Marche.java
+++ src/Marche.java
@@ -16,7 +16,7 @@ public class Marche {
 				{"Clémentine", "Datte", "Grenade", "Kaki", "Kiwi", "Mandarine", "Orange", "Pamplemousse", "Poire","Pomme", 		//  <- Noms
 				"Carrote", "Choux de Bruxelles", "Chou vert", "Courge butternut", "Endive", "Epinard", "Poireau", "Potiron", "Radis noir", "Salsifis"},	
 				{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},	// <- Id
-				{6, 4, 3, 3, 5, 6, 8, 8, 5, 8, 7, 4, 12, 6, 5, 4, 5, 6, 10, 0},		//  <- Stock
+				{6, 4, 3, 3, 5, 6, 8, 8, 5, 8, 7, 4, 12, 6, 5, 4, 5, 6, 10, 3},		//  <- Stock
 				{2.90, 7, 3.50, 4.50, 3.50, 2.80, 1.50, 2, 2.50, 1.50, 1.30, 4, 2.50, 2.50, 2.50, 2.60, 1.20, 2.50, 5, 2.50},	//  <- Prix
 				{true, true, true, true, true, true, true, false, true, true, true, true, false, false, true, true, true, false, false, true},	//  <- Prix au kg ?
 		};
--- src/Update.java
+++ src/Update.java
@@ -18,4 +18,45 @@ public class Update {
 		
 		return merchandiseList;
 	}
+	// Méthode qui permet de mettre à jour le panier du client :
+	public static void updateCart(ArrayList<HashMap<String, Object>> merchandiseList, HashMap<String, Object> customerInfos, int merchandiseId, double merchandiseQuantity) {
+		// On viens récupérer la HashMap de la marchandise à ajouter dans le panier :
+		HashMap<String, Object> merchandiseToAdd = merchandiseList.get(merchandiseId - 1);
+		// On viens récupérer la HashMap qui fait office de panier :
+		Object cartObject = customerInfos.get("Cart");
+		@SuppressWarnings("unchecked")
+		HashMap<String, Object> cart = (HashMap<String, Object>) cartObject;
+		
+		
+		// Opération de casting explicite pour convertir notre valeur string encapsuler dans l'objet :
+		Object nameMerchandiseToAddObject = merchandiseToAdd.get("Nom");
+		String nameMerchandiseToAddString = (String) nameMerchandiseToAddObject;
+		
+		// Opération de casting explicite pour convertir notre valeur boolean encapsuler dans l'objet :
+		Object pricePerKgObject = merchandiseToAdd.get("PrixAuKg");
+		boolean pricePerKgBool = (boolean) pricePerKgObject;
+		
+		// Opération de casting explicite pour convertir notre valeur numérique encapsuler dans l'objet :
+		Object priceMerchandiseToAddObject = merchandiseToAdd.get("Prix");
+		double priceMerchandiseToAddDouble = ((Number) priceMerchandiseToAddObject).doubleValue();
+		
+		// On calcule le prix de la marchandise sélectionner x sa quantité :
+		double quantityPriceToAdd = priceMerchandiseToAddDouble * merchandiseQuantity;
+		
+		// On calcule le prix total du panier :
+		Object totalPriceCartObject = customerInfos.get("TotalPrice");
+		double totalPriceCartDouble = ((Number) totalPriceCartObject).doubleValue();
+		double totalPriceCartUpdated = totalPriceCartDouble + quantityPriceToAdd;
+		
+		// On met à jour les informations du client :
+		// Le prix total :
+		customerInfos.put("TotalPrice", totalPriceCartUpdated);
+		// Le panier :
+		cart.put("NameItem", nameMerchandiseToAddString);
+		cart.put("QuantityItem", merchandiseQuantity);
+		cart.put("PricePerKg", pricePerKgBool);
+		customerInfos.put("Cart", cart);
+		System.out.println("Infos client : " + customerInfos);
+		System.out.println("Panier client : " + cart);
+	}
 }
