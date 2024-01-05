25326e587efd7111700e96c1ec51684bdc20c320
Arthur Gibert <arthurgibert@free.fr>
2024-01-05 11:27:31 +0100

    Ajout de la méthode checkMerchandiseQuantity qui permet de vérifier la saisie utilisateur concernant la quantité du produit voulue + vérification par rapport au stock restant de ce produit.

--- src/Check.java
+++ src/Check.java
@@ -61,5 +61,60 @@ public class Check {
 		}
 		return userChoice;
 	}
+	// Méthode qui permet de vérifier la saisie utilisateur concernant la quantité voulue, par rapport au stock restant :
+	public static double checkMerchandiseQuantity(Scanner scanner, ArrayList<HashMap<String, Object>> merchandiseList, int merchandiseId) {
+		double userChoiceQuantity;
+		
+		// On viens récupérer la HashMap de la marchandise sélectionner :
+		HashMap<String, Object> merchandiseToCheck = merchandiseList.get(merchandiseId - 1);
+		
+		// Opération de casting explicite pour convertir notre valeur numérique encapsuler dans l'objet :
+		Object stockToCheckObject = merchandiseToCheck.get("Stock");
+		double stockToCheckDouble = ((Number) stockToCheckObject).doubleValue();
+		
+		// Opération de casting explicite pour convertir notre valeur boolean encapsuler dans l'objet :
+		Object pricePerKgObject = merchandiseToCheck.get("PrixAuKg");
+		boolean pricePerKgBool = (boolean) pricePerKgObject;
+		
+		while(true) {
+			// Si le produit se vend à l'unité, alors :
+			if(pricePerKgBool == false) {
+				if(scanner.hasNextInt()) {
+					userChoiceQuantity = scanner.nextInt();
+					// Si la quantité choisie est égale à 0 ou supérieur au stock du produit sélectionner :
+					if(userChoiceQuantity <= 0 || userChoiceQuantity > stockToCheckDouble) {
+						System.out.println("Nous n'avons pas cette quantité en stock pour ce produit !"); 
+						System.out.print("Veuillez choisir une quantité correcte (Stock restant :" + stockToCheckDouble + " pièce(s)" + ") :");
+					// Sinon :
+					} else {
+						break;
+					}
+				// Sinon :
+				} else {
+					System.out.print("Ce produit se vend à l'unité, veuillez choisir une quantité correcte :");
+					scanner.next();
+				}
+			// Sinon, si c'est un produit qui se vend au kg, alors :
+			} else {
+				if(scanner.hasNextDouble()) {
+					userChoiceQuantity = scanner.nextDouble();
+					// Si la quantité choisie est égale à 0 ou supérieur au stock du produit sélectionner :
+					if(userChoiceQuantity <= 0 || userChoiceQuantity > stockToCheckDouble) {
+						System.out.println("Nous n'avons pas cette quantité en stock pour ce produit !"); 
+						System.out.print("Veuillez choisir une quantité correcte (Stock restant :" + stockToCheckDouble + " Kg" + ") :");
+					// Sinon :
+					} else {
+						break;
+					}
+				// Sinon :
+				} else {
+					System.out.print("Ce produit se vend au kg, veuillez choisir une quantité correcte :");
+					scanner.next();
+				}
+			}
+			
+		}
+		return userChoiceQuantity;
+	}
 }
 
