7df0beca9f2ff4c6e17a8b5154374d3d61e6638d
Arthur Gibert <arthurgibert@free.fr>
2024-01-05 14:19:23 +0100

    Ajout de la boucle while dans la méthode purchaseMerchandise pour boucler si jamais le client n'à pas finit ces achats.

--- src/Purchase.java
+++ src/Purchase.java
@@ -7,26 +7,33 @@ public class Purchase {
 	// Elle met à jour la liste des produits et du panier :
 	// Et boucle si jamais le client n'à pas finis ces achats :
 	public static void purchaseMerchandise(Scanner scanner, ArrayList<HashMap<String, Object>> merchandiseList, HashMap<String, Object> customerInfos) {
-		// On affiche la liste des produits au client :
-		Display.displayMerchandiseList(merchandiseList);
-		
-		// On récupère l'id du produit ainsi que sa quantité :
-		System.out.println();
-		System.out.println("Veuillez saisir l'ID du produit que vous souhaitez ajouter au panier :");
-		
-		// On apelle la méthode checkMerchandiseId pour tester la saisie utilisateur :
-		int userChoiceId = Check.checkMerchandiseId(scanner, merchandiseList);
-		System.out.println("Veuillez saisir la quantité du produit que vous souhaitez ajouter au panier :");
-		
-		// On apelle la méthode checkMerchandiseQuantity pour tester la saisie utilisateur :
-		double userChoiceQuantity = Check.checkMerchandiseQuantity(scanner, merchandiseList, userChoiceId);
-		
-		// On assigne à merchadiseList la liste mise à jour retourner par notre méthode updateMerchandiseList :
-		merchandiseList = Update.updateMerchandiseList(merchandiseList, userChoiceId, userChoiceQuantity);
-		
-		// On met à jour le panier du client :
-		customerInfos = Update.updateCart(merchandiseList, customerInfos, userChoiceId, userChoiceQuantity);
-		
-		System.out.print("Avez-vous terminé vos achats ? (Oui/Non) :");
+		while(true) {
+			// On affiche la liste des produits au client :
+			Display.displayMerchandiseList(merchandiseList);
+			
+			// On récupère l'id du produit ainsi que sa quantité :
+			System.out.println();
+			System.out.print("Veuillez saisir l'ID du produit que vous souhaitez ajouter au panier : ");
+			
+			// On apelle la méthode checkMerchandiseId pour tester la saisie utilisateur :
+			int userChoiceId = Check.checkMerchandiseId(scanner, merchandiseList);
+			System.out.print("Veuillez saisir la quantité du produit que vous souhaitez ajouter au panier : ");
+			
+			// On apelle la méthode checkMerchandiseQuantity pour tester la saisie utilisateur :
+			double userChoiceQuantity = Check.checkMerchandiseQuantity(scanner, merchandiseList, userChoiceId);
+			
+			// On assigne à merchadiseList la liste mise à jour retourner par notre méthode updateMerchandiseList :
+			merchandiseList = Update.updateMerchandiseList(merchandiseList, userChoiceId, userChoiceQuantity);
+			
+			// On met à jour le panier du client :
+			customerInfos = Update.updateCart(merchandiseList, customerInfos, userChoiceId, userChoiceQuantity);
+			
+			System.out.print("Avez-vous terminé vos achats ? (Oui/Non) : ");
+			String userChoice = scanner.next();
+			if(userChoice.toLowerCase().equals("oui")) {
+				System.out.println("Achats terminés !");
+				break;
+			}
+		}
 	}
 }
